864f88f475e3645812f4a6e10e8c09953aecee1d
Moses-main <moseschizaram8@gmail.com>
2025-08-28 06:52:41 +0100

    fix the signup render

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { toast } from 'react-toastify';
-import { FaUser, FaEnvelope, FaLock, FaWallet, FaUserMd, FaUserInjured } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaLock, FaWallet, FaUserMd, FaUserInjured, FaEye, FaEyeSlash } from 'react-icons/fa';
 import api from '../../services/api';
 
 // Validation schema using Yup
